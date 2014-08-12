ef472ef412d1e3dd24bdbfc7ef3ac4bb09e68407
Ningxin Hu <ningxin.hu@intel.com>
2014-08-13 03:13:21 +0800

    Implement the average bench.

---
+++ average.cc
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <time.h>
+#include <xmmintrin.h>
+
+float average(float *src, int len) {
+  float sum = 0.0;
+  for (int i = 0; i < len; ++i) {
+  sum = sum + src[i];
+}
+  return sum/len;
+}
+
+float simdAverage(float *src, int len) {
+  __m128 sumx4 = _mm_setzero_ps();
+  for (int i = 0; i < len; i += 4) {
+    __m128 v = _mm_load_ps(src);
+    sumx4 = _mm_add_ps(sumx4, v);
+    src += 4;
+  }
+  float sumx4_mem[4];
+  float *sumx4_ptr = sumx4_mem;
+  _mm_store_ps(sumx4_ptr, sumx4);
+  return (sumx4_mem[0] + sumx4_mem[1] +
+          sumx4_mem[2] + sumx4_mem[3])/len;
+}
+
+void initArray(float *src, int len) {
+	for (int i = 0; i < len; ++i) {
+    src[i] = 0.1 * i;
+  }
+}
+
+int main() {
+  const int len = 100000;
+  const int iterations = 100000;
+  float src[len];
+  float result = 0.0;
+  clock_t start, end;
+  double cpu_time_used, cpu_time_used1;
+
+  initArray(src, len);
+  printf("init done\n");
+
+  start = clock();
+  for (int i = 0; i < iterations; ++i)
+    result = average(src, len);
+  end = clock();
+  cpu_time_used = ((double) (end - start));
+  printf("average result: %f %f\n", result, cpu_time_used);
+
+  start = clock();
+  for (int i = 0; i < iterations; ++i)
+    result = simdAverage(src, len);
+  end = clock();
+  cpu_time_used1 = ((double) (end - start));
+  printf("averagex4 result: %f %f\n", result, cpu_time_used1);
+
+  printf("speed up: %f\n", cpu_time_used/cpu_time_used1);
+}
